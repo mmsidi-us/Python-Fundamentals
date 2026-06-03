f6203324f6781fe87397303afb781921a591d162
mmsidi-us <mmsidiabd@gmail.com>
2026-06-03 10:56:54 -0400

    Initial commit

---
+++ about_me.py
@@ -0,0 +1,18 @@
+first_name= 'Mohamed Mahmoud'
+last_name= 'Sidi Abdoulah'
+age= 48
+city= 'Florence'
+previous_career= 'Amazon Associate'
+print('=' * 40)
+print('student information')
+print('=' * 40)
+print(f"Name:  {first_name} {last_name}")
+print(f'Age: {age}')
+print(f"Previous Career: {previous_career}")
+print(f"\nWhy I'm learning to code:")
+print(f"I want to build AI tools that help small businesses.")
+print('=' * 40)
+
+      
+
+    
